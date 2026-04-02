9ef4ff80466f24873f29e657cc24419cc9e9df97
th1nde <arnaud@monel.fr>
2026-04-02 16:16:30 +0200

    Début ajout modal modif user

--- src/app/feature/users/user/user.ts
+++ src/app/feature/users/user/user.ts
@@ -1,5 +1,5 @@
 import { OnInit, signal } from '@angular/core';
-import { User } from '../../../core/services/user';
+import { UserService } from '../../../core/services/user';
 import { Component  } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -8,60 +8,66 @@ import { AuthService } from '../../../core/services/auth';
 import { ProfilAnnonce } from './profil-annonce/profil-annonce';
 import { CookieService } from 'ngx-cookie-service';
 import { HttpHeaders } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
-  imports: [CommonModule, RouterLink, ProfilAnnonce],
+  imports: [CommonModule, RouterLink, ProfilAnnonce, FormsModule],
   templateUrl: './user.html',
   styleUrls: ['./user.css'],
 })
-export class Profil implements OnInit {
+export class User implements OnInit {
   isEditModalOpen = false;
   tempNom: string = '';
   tempprenom: string = '';
   tempemail: string = '';
-  tempphotoProfil: string = '';
+  tempphotoProfil: Text | string = '';
   temppassword: string = '';
 
   constructor(
     private cookiesService: CookieService,
     public authService: AuthService,
     public app: App,
+    private userService: UserService
   ) {}
 
   ngOnInit(): void {
-
-    function openEditModal() {
-      const p = this.currentUser.profil || this.currentUser.result.profil;
-      this.tempNom = p.Nom || '';
-      this.tempprenom = p.prenom || '';
-      this.tempemail = p.email || '';
-      this.temppassword = p.password || '';
-      this.tempphotoProfil = p.photoProfil || '';
-      this.isEditModalOpen = true;
     }
 
-    // function saveProfil() {
-    //   const token = cookieStore.get('voisinConnecterToken');
-    //   if (!token) return;
-    //
-    //   const headers = new HttpHeaders({ Autorization: `Bearer ${token}` });
+    openEditModal() {
+    const p = this.app.currentUser;
+    this.tempNom = p.Nom || '';
+    this.tempprenom = p.prenom || '';
+    this.tempemail = p.email || '';
+    this.temppassword = p.password || '';
+    this.tempphotoProfil = p.photoProfil || '';
+    this.isEditModalOpen = true;
+    }
+    saveProfil() {
+      const token = cookieStore.get('voisinConnecterToken');
+      if (!token) return;
 
-      // const NomRequest = this.http.post(
-      //   `${this.baseUrl}/route/...`,
-      //   { Nom: this.tempNom },
-      //   { headers },
-      // )
-
-      // A mettre les routes pour modif profil dand userServices.ts
-
+      const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
+    }
+    updateLocalData(p: string, value: string) {
+      if (this.app.currentUser) this.app.currentUser[p] = value;
     }
 
-    // function updateProfil() {
-    //
-    //   this.updateLocalData('Nom', this.tempNom);
-    // }
+    forkJoin([Nom]){ next: () => {
+      const nom = this.tempNom.startsWith('data:');
 
-  // }
+      this.updateLocalData('nom', this.tempNom);
+
+      this.authService.updateUser({ ...this.app.currentUser });
+      this.isEditModalOpen = false;
+      console.log('Profil mis à jour et synchroniser !');
+    }
+  }
+
+      updateProfil() {
+        this.updateLocalData('Nom', this.tempNom);
+      }
+    // creation annonce
+
 }
 
