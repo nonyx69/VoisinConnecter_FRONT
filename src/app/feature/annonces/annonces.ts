b56b413955199f99818aac3851e0021f4b421b2f
Takeshydo <lacoque.mathys.pro@gmail.com>
2026-04-03 03:16:45 +0200

    Canvas Create Annonce Good + Status

--- src/app/feature/annonces/annonces.ts
+++ src/app/feature/annonces/annonces.ts
@@ -11,35 +11,46 @@ import { ProductModel} from '../../shared/models/product.model';
   styleUrl: './annonces.css',
 })
 export class Annonces {
+  //Tableau Catégories
+  category = [
+    { id: 1, name: 'Bricolage' },
+    { id: 2, name: 'Jardinage' },
+    { id: 3, name: 'Aide' },
+    { id: 4, name: 'Animaux' },
+    { id: 5, name: 'Informatique' },
+  ];
 
-  constructor(private app: App,
-              private productService: ProductService,
-              private router: Router,) {
-  }
+  constructor(
+    private app: App,
+    private productService: ProductService,
+    private router: Router,
+  ) {}
 
   protected products = signal<ProductModel[]>([]);
 
   ngOnInit() {
-    this.productService.getAll(this.app.urlAPI(), this.app.createCORS()).subscribe((reponseProductAll ) => {
-      if (reponseProductAll.status == 'success') {
-        this.products.set(reponseProductAll.result);
-      }
-    })
+    this.productService
+      .getAll(this.app.urlAPI(), this.app.createCORS())
+      .subscribe((reponseProductAll) => {
+        if (reponseProductAll.status == 'success') {
+          this.products.set(reponseProductAll.result);
+        }
+      });
   }
-
 
   Filtre(category: string) {
-    this.productService.filterBy(category, this.app.urlAPI(), this.app.createCORS()).subscribe(reponseProductfiltre => {
-      if (reponseProductfiltre.status == "success") {
-        this.products.set(reponseProductfiltre.result);
-      }
-      else{
-        console.log(reponseProductfiltre.status);
-      }
-    })
+    this.productService
+      .filterBy(category, this.app.urlAPI(), this.app.createCORS())
+      .subscribe((reponseProductfiltre) => {
+        if (reponseProductfiltre.status == 'success') {
+          this.products.set(reponseProductfiltre.result);
+        } else {
+          console.log(reponseProductfiltre.status);
+        }
+      });
   }
 
-  All(){
+  All() {
     this.productService
       .getAll(this.app.urlAPI(), this.app.createCORS())
       .subscribe((reponseProductAll) => {
